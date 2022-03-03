8a4ea416bfaed8cf7de9fd88008fe972ea348c76
RODY DA CRUZ <rodylag@outlook.com>
2022-03-03 17:11:41 -0100

    was a lot of things

--- cvww.js
+++ cvww.js
@@ -14,8 +14,20 @@ const thumbsGlider = new Glider(document.querySelector('.glider'), {
 
 $thumbs.forEach(btn => {
 	btn.addEventListener('click', e => {
+		clearInterval(interval);
+		startinterval();
 		const index = e.target.getAttribute('data-glider-index');
 
 		thumbsGlider.scrollItem(index, true);
 	});
 });
+var interval
+function startinterval(){
+	i = 0;
+interval = window.setInterval(function(){
+	i = i + 1;
+	if(i==9)i=0;
+    thumbsGlider.scrollItem(i, true);
+}, 4000);
+}
+startinterval();
